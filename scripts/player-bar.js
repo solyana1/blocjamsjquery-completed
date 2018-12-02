9771ff98b589ea31eafb87413d9fadb7eba51c11
Solyana Tafesse <sg.taffe@gmail.com>
2018-12-02 13:23:15 -0500

    assignment 11 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -23,7 +23,7 @@
   });
 
   setInterval( ()=> {
-    if (player.playState) !== 'playing' {return; }
+    if (player.playState !== 'playing') {return; }
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
